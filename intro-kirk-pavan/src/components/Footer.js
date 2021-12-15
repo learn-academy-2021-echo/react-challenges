140377c2f849538b9a1856762e279b6868d0fcdb
Punjabi2021 <93889575+Punjabi2021@users.noreply.github.com>
2021-12-15 12:19:49 -0800

    pushed the first few lines of code

---
+++ intro-kirk-pavan/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Footer extends Component{
+  render(){
+    return(
+      <>
+        <h2>
+          Echo 2021 Squaaad!
+        </h2>
+      </>
+    )
+  }
+}
+
+export default Footer
---
+++ intro-kirk-pavan/src/components/Profile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Header extends Component{
+  render(){
+    return(
+      <>
+        <h3>
+Pavan and Kirk will bring the best of the best to the tier list.
+        </h3>
+      </>
+    )
+  }
+}
+
+export default Header
