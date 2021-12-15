fa06dcdba043bf6b4e4ee69a5eea68ae9c28a821
kmsmith5 <kmsmith5@gmail.com>
2021-12-15 14:58:48 -0500

    completed challenges

---
+++ intro-chris-kevin/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component{
+  render(){
+    return(
+      <>
+      <h2>Echo 2021</h2>
+      </>
+    )
+  }
+}
+
+export default Footer
---
+++ intro-chris-kevin/src/components/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Profile extends Component{
+  render(){
+    return(
+      <>
+      <p>Kevin: Master Dev</p>
+      <p>Chris: Master Dev</p>
+      </>
+    )
+  }
+}
+
+export default Profile
