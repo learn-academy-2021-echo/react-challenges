72ae41566376759c97d8f03964d135ea8f4b1c3c
yannsf29 <yann.crennsf@gmail.com>
2021-12-15 16:49:06 -0800

    yann and mikka color box

---
+++ state-yann-mikka/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './App.css'
+// import './components/box.js'
+
+
+
+class App extends Component {
+  render() {
+    return (
+    <>
+       <h1> Color Box </h1>
+
+
+<div class= "box">
+
+</div>
+
+
+);
+  }
+
+
+}
+
+
+export default App
---
+++ state-yann-mikka/src/components/box.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Box extends Component {
+  render() {
+    return (
+    <>
+       <h1> Color Box </h1>
+
+
+    </>
+    )
+  }
+
+
+}
+
+
+export default Box
