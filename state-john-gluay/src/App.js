755d9ba3517433f438eb599ed54a70c974b312bc
gluay88 <p.wantagaew@gmail.com>
2021-12-16 09:21:36 -0800

    Stateofmind

---
+++ state-john-gluay/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Header from './components/Header'
+import Box from './components/Box'
+import './App.css'
+
+
+class App extends Component {
+  render() {
+    return (
+     <>
+     <Header />
+     <Box />
+     </>
+    )
+  }
+}
+
+export default App
---
+++ state-john-gluay/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+     <>
+     <h1>Color Box Challenge</h1>
+     <p>John and Gluay</p>
+     </>
+    )
+  }
+}
+
+export default Header
