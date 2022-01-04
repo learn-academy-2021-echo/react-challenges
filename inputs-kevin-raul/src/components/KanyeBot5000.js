11e6be5fbad7fd1878783264213fe42da9cc3ac9
kmsmith5 <kmsmith5@gmail.com>
2022-01-04 17:56:51 -0500

    working on bad robot

---
+++ inputs-kevin-raul/src/components/KanyeBot5000.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class KanyeBot5000 extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <>
+      <h2>I'm going to let you finish, but Beyonce is {this.props.words}</h2>
+
+      </>
+    )
+  }
+}
+
+export default KanyeBot5000;
