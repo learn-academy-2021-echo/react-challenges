56f471d58241e9e75d94d54b26e078c6cf4257f5
gitgood-ENT <ward.noahd@gmail.com>
2022-01-31 16:02:00 -0800

    completed location api

---
+++ fetch-noah-samantha/src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      city: null,
+      state: null,
+    }
+  }
+
+  componentDidMount(){
+    
+    fetch("https://ipapi.co/json/")
+      .then(response => response.json())
+      .then(payload => this.setState({state: payload.region_code, city: payload.city}))
+      .catch(error => console.log(error))
+  }
+
+  // getLocation = () => {
+  //   fetch("https://ipapi.co/json/")
+  //   .then(function(response) {
+  //     response.json().then(jsonData => {
+  //       console.log(jsonData);
+  //     });
+  //   })
+  //   .catch(function(error) {
+  //     console.log(error)
+  //   });
+  // }
+
+
+  render() {
+    return <div>
+      <div  className='locator'>
+      {/* <button onClick={this.getLocation}>
+        Click here if you're lost 
+      </button> */}
+      <h2>Are you lost?</h2>
+      <h3>{this.state.state != null ? `You are in ${this.state.city}, ${this.state.state}.` : null}</h3>
+    </div>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <p> hi</p>
+    </div>;
+  }
+}
+
+
