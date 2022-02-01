b7fa895c04140b35357b162010c5f0dbcc3a9469
Samanthap803b <samanthap803b@gmail.com>
2022-01-31 16:37:14 -0800

    done with strech challenge

--- fetch-noah-samantha/src/App.js
+++ fetch-noah-samantha/src/App.js
@@ -7,15 +7,20 @@ export default class App extends Component {
     this.state = {
       city: null,
       state: null,
+      nasaImage: null,
     }
   }
 
   componentDidMount(){
-    
+
     fetch("https://ipapi.co/json/")
       .then(response => response.json())
       .then(payload => this.setState({state: payload.region_code, city: payload.city}))
       .catch(error => console.log(error))
+
+    fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+     .then(response => response.json())
+     .then(payload => this.setState({nasaImage: payload.url}))
   }
 
   // getLocation = () => {
@@ -35,18 +40,17 @@ export default class App extends Component {
     return <div>
       <div  className='locator'>
       {/* <button onClick={this.getLocation}>
-        Click here if you're lost 
+        Click here if you're lost
       </button> */}
       <h2>Are you lost?</h2>
       <h3>{this.state.state != null ? `You are in ${this.state.city}, ${this.state.state}.` : null}</h3>
     </div>
     <br/>
     <br/>
+    <img src={this.state.nasaImage}></img>
     <br/>
     <br/>
     <p> hi</p>
     </div>;
   }
 }
-
-
