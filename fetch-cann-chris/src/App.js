d2b3d4996aaa2dcfcd7668f5662d7e56d5ceb4b6
yannsf29 <yann.crennsf@gmail.com>
2022-01-31 16:18:34 -0800

    fetch completed

---
+++ fetch-cann-chris/src/App.js
@@ -0,0 +1,36 @@
+
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      locationData:""
+
+
+    }
+  }
+
+  getLocation = () => {
+    fetch("https://ipapi.co/json/")
+    .then (response => response.json())
+    .then (payload => this.setState({locationData: payload}))
+  }
+  render() {
+    return (
+
+   <>
+   <h1>Hello World</h1>
+   <button onClick={this.getLocation}>Get Location Data</button>
+   <p>Your Ip: {this.state.locationData.ip}</p>
+   <p>Your City: {this.state.locationData.city}</p>
+   
+   
+   </>    
+    );
+  }
+}
+
+
+export default App;
