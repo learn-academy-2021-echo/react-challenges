6d462e47bf7e0f666d5f278f7e842040ee6fcad8
Punjabi2021 <93889575+Punjabi2021@users.noreply.github.com>
2022-01-31 23:01:46 -0800

    still working

--- fetch/src/App.js
+++ fetch/src/App.js
@@ -3,7 +3,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: "",
+      locationData: "",
     };
   }
 
@@ -11,17 +11,16 @@ class App extends Component {
     fetch(
       "https://api.nasa.gov/planetary/apod?api_key=ggQXskXCOG4aH1iORntodMlJpp2rWPy1k4bvAPaI"
     )
-      .then((responce) => responce.json())
+      .then((response) => response.json())
       .then((payload) => this.setState({ locationData: payload }));
   };
 
   render() {
+    console.log(this.state.locationData);
     return (
       <>
         <h1>hello world</h1>
-        <button onclick={this.getlocation}>Click Here for location</button>
-        <p>Your IP:{this.state.location.ip}</p>
-        <p>Your Current Location:{this.state.locationData.city}</p>
+        <button onClick={this.getlocation}>Click Here for location</button>
       </>
     );
   }
